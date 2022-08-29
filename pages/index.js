507c52760eaa766ca77779a3e9eb0e8488653fc9
fry <fry.land@hotmail.fr>
2022-08-29 22:00:44 +0200

    adding tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello There!!!!</h1>
-    </div>
+    <>
+      <Head>
+        <title>Fry Blog</title>
+        <link rel='shortcut icon' href='favicon.ico' />
+      </Head>
+      <div>
+        <h1 className='text-4xl'>Hello There!!!!</h1>
+      </div>
+    </>
   );
 }
